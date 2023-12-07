809e4da0b09faa6b4485c8587390b2738dc8c162
Niklas Mohrin <dev@niklasmohrin.de>
2023-12-07 12:02:13 +0100

    Add flake files

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "AOC 2023";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            llvmPackages_6.libcxxabi
+            (python311.withPackages (ps: with ps; [
+              pip
+              python-lsp-server
+              python-lsp-black
+              pylsp-mypy
+              pylsp-rope
+              python-lsp-ruff
+              ujson
+
+              more-itertools
+            ]))
+
+            pypy310
+          ];
+        };
+      });
+}
